59d6471e93d60dd5218e837c07b3e4b309db83ed
atcvetkov <63304837+atcvetkov@users.noreply.github.com>
2021-12-22 03:15:00 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+function effect_7 () {
+    for (let bright = 0; bright <= 200; bright++) {
+        ring.setBrightness(bright)
+        ring.showRainbow(1, 360)
+        basic.pause(5)
+        ring.clear()
+    }
+    for (let bright = 0; bright <= 200; bright++) {
+        ring.setBrightness(200 - bright)
+        ring.showRainbow(1, 360)
+        basic.pause(5)
+        ring.clear()
+    }
+}
 function effect_1 () {
     basic.showNumber(1)
     colors = [neopixel.colors(NeoPixelColors.Red), neopixel.colors(NeoPixelColors.Green), neopixel.colors(NeoPixelColors.Blue)]
@@ -24,6 +38,36 @@ function effect_2 () {
         ring.clear()
     }
 }
+function effect_5 () {
+    ring.setBrightness(10)
+    ring.setPixelColor(randint(0, 23), neopixel.colors(NeoPixelColors.Orange))
+    ring.setPixelColor(randint(0, 23), neopixel.colors(NeoPixelColors.Purple))
+    ring.setPixelColor(randint(0, 23), neopixel.colors(NeoPixelColors.Red))
+    ring.setPixelColor(randint(0, 23), neopixel.colors(NeoPixelColors.Yellow))
+    ring.setPixelColor(randint(0, 23), neopixel.colors(NeoPixelColors.Blue))
+    ring.setPixelColor(randint(0, 23), neopixel.colors(NeoPixelColors.Green))
+    ring.show()
+    basic.pause(100)
+    ring.clear()
+}
+function effect_6 () {
+    led_id_1 = randint(0, 23)
+    led_id_2 = randint(0, 23)
+    led_id_3 = randint(0, 23)
+    led_id_4 = randint(0, 23)
+    led_id_5 = randint(0, 23)
+    for (let bright = 0; bright <= 200; bright++) {
+        ring.setBrightness(200 - bright)
+        ring.setPixelColor(led_id_1, neopixel.colors(NeoPixelColors.Red))
+        ring.setPixelColor(led_id_2, neopixel.colors(NeoPixelColors.Green))
+        ring.setPixelColor(led_id_3, neopixel.colors(NeoPixelColors.Blue))
+        ring.setPixelColor(led_id_4, neopixel.colors(NeoPixelColors.Purple))
+        ring.setPixelColor(led_id_5, neopixel.colors(NeoPixelColors.Yellow))
+        ring.show()
+        basic.pause(1)
+        ring.clear()
+    }
+}
 function effect_3 () {
     ring.setBrightness(10)
     for (let index = 0; index <= 23; index++) {
@@ -39,9 +83,20 @@ function effect_3 () {
         ring.clear()
     }
 }
+function effect_multiple () {
+    for (let index = 0; index < 3; index++) {
+        effect_7()
+    }
+    for (let index = 0; index < 5; index++) {
+        effect_6()
+    }
+    for (let index = 0; index < 10; index++) {
+        effect_4()
+    }
+}
 function effect_4 () {
-    ring.setBrightness(10)
     for (let index = 0; index <= 23; index++) {
+        ring.setBrightness(input.soundLevel() + 10)
         color_10_index = index
         color_11_index = color_10_index + 1
         color_12_index = color_10_index + 2
@@ -63,14 +118,14 @@ function effect_4 () {
         if (color_22_index > 23) {
             color_22_index = color_22_index - 24
         }
-        ring.setPixelColor(color_10_index, neopixel.colors(NeoPixelColors.Green))
-        ring.setPixelColor(color_11_index, neopixel.colors(NeoPixelColors.Green))
+        ring.setPixelColor(color_10_index, neopixel.rgb(0, 50, 0))
+        ring.setPixelColor(color_11_index, neopixel.rgb(0, 150, 0))
         ring.setPixelColor(color_12_index, neopixel.colors(NeoPixelColors.Green))
-        ring.setPixelColor(color_20_index, neopixel.colors(NeoPixelColors.Red))
-        ring.setPixelColor(color_21_index, neopixel.colors(NeoPixelColors.Red))
+        ring.setPixelColor(color_20_index, neopixel.rgb(50, 0, 0))
+        ring.setPixelColor(color_21_index, neopixel.rgb(150, 0, 0))
         ring.setPixelColor(color_22_index, neopixel.colors(NeoPixelColors.Red))
         ring.show()
-        basic.pause(100)
+        basic.pause(30)
         ring.clear()
     }
 }
@@ -82,6 +137,11 @@ let color_11_index = 0
 let color_10_index = 0
 let color_2_index = 0
 let color_1_index = 0
+let led_id_5 = 0
+let led_id_4 = 0
+let led_id_3 = 0
+let led_id_2 = 0
+let led_id_1 = 0
 let range2: neopixel.Strip = null
 let one = 0
 let list: number[] = []
@@ -89,5 +149,5 @@ let colors: number[] = []
 let ring: neopixel.Strip = null
 ring = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
 basic.forever(function () {
-    effect_4()
+    effect_multiple()
 })
